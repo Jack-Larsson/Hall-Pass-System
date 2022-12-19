bcdc8fac0784b0f8fe20fdf5d2bf9ffd8a143aee
Jack-Larsson <jacklarsson23@gmail.com>
2022-12-19 10:37:09 -0600

    It works but connect to sheets and rosters

--- .vs/IA/Restroom Log/Restroom_Log.py
+++ .vs/IA/Restroom Log/Restroom_Log.py
@@ -1,10 +1,10 @@
 
 from tkinter import *
 from time import *
+import SheetsScript as SS
 
 
 #pi screen is 800x480 pixels
-#https://stackoverflow.com/questions/65527158/how-to-get-variable-from-a-class-to-another-tkinter-window
 class SelectStudent(Tk):
 
 
@@ -12,7 +12,8 @@ class SelectStudent(Tk):
         "john cena",
         "mrpoopybutthole",
         "Xi jinping",
-        "Nathan Mardanov",
+        "Lathan Lardanov",
+        "Wathan Wardanov",
         "L bozo",
         "a bozo",
         "the bozo",
@@ -27,15 +28,44 @@ class SelectStudent(Tk):
         "Alex Loser",
         "GrayGray",
         "Ken"
+        "Wewis hamiltonn",
+        "Wionel pessi",
+        "Lenaldo",
+        "Wakaka",
+        "Wlex Woser",
+        "WrayWray",
+        "Wen",
+        "lewis hamiltonn",
+        "lionel pessi",
+        "penaldo",
+        "lakaka",
+        "Alex Loser",
+        "GrayGray",
+        "Ken"
+        "Wewis hamiltonn",
+        "Wionel pessi",
+        "Lenaldo",
+        "Wakaka",
+        "Wlex Woser",
+        "WrayWray",
+        "Wen",
+        "john cena",
+        "mrpoopybutthole",
+        "Xi jinping",
+        "Lathan Lardanov",
+        "Wathan Wardanov",
+        "L bozo",
+        "a bozo",
+        "the bozo",
+        "Fred",
+        "Konstantin"
         ]
 
     def __init__(self):
         super().__init__()
-        #self.frame = Frame(self.root)
-        self.attributes('-fullscreen', True)
-        #self.root.geometry("200x200")
-        #Label(self.root, bg = "blue", text = "at least it isn't completely broken").pack
-        #self.button_rename = Button(self.root, text = "New window").pack()
+        #self.attributes('-fullscreen', True)
+        self.geometry("800x480")
+        self.configure(bg='White')
         self.makeButtons()
         self.bind('<Escape>',lambda e: window.destroy())
 
@@ -50,15 +80,16 @@ class SelectStudent(Tk):
         buttons = []
 
         for j in range(len(self.students)):
-            buttons.append(Button(self,text = self.students[j], width = 10, height = 4, bg = "Blue", command = lambda j=j: self.OpenNewWindow(self.students[j])).grid(row = r, column = c, padx = 10, pady = 10))
+            buttons.append(Button(self,text = self.students[j], width = 13, height = 3, bg = "Navy", fg= "White", font=('Arial bold',9), borderwidth= 0,  command = lambda j=j: self.OpenNewWindow(self.students[j])).grid(row = r, column = c, padx = 8, pady = 8))
             c+=1
-            if (c==5):
+            if (c==7):
                 c=0
                 r+=1
 
 
     def OpenNewWindow(self, student):
         self.withdraw()
+        SS.printstudent(student)
         Thanks(self, student)
         
 
@@ -68,11 +99,12 @@ class Thanks(Tk):
         super().__init__()
 
         self.parent = parent
-        self.attributes('-fullscreen', True)
-        
-        Label(self,bg = "white", fg= "blue", text = "Thank You!\nHave Fun", font=('Helvetica bold',200)).grid(row=1, column= 1)
+        self.geometry("800x480")
+        #self.attributes('-fullscreen', True)
+        self.configure(bg='Navy')
+        Label(self, fg= "White",bg='Navy', text = "Thank You!\nHave Fun", font=('Helvetica bold',115)).grid(row=1, column= 1)
        
-        print("Thanks test"+ student_name)
+        #print("Thanks test"+ student_name)
 
         self.after(5000, lambda: self.OpenNewWindow(student_name))
 
@@ -90,11 +122,22 @@ class StudentOut(Tk):
         super().__init__()
 
         self.parent = parent
-        self.attributes('-fullscreen', True)
+        self.geometry("800x480")
+        #self.attributes('-fullscreen', True)
 
-        print("new screen test")
+        self.configure(bg='Navy')
 
-        Label(self,bg = "white", fg= "blue", text = student_name + " is currently out\nThank you for your patience", font=('Helvetica bold',100)).grid(row= 1, column=1)
+        #print("new screen test")
+
+        IsOut = student_name
+        IsOut = IsOut.replace("\n", " ")
+
+        goodbye= Label(self, fg= "White",bg='Navy', text = IsOut + " \nis currently out\nThank you for\nyour patience", font=('Helvetica bold',60))
+        goodbye.grid(row= 1, column=1)
+
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
 
         self.bind('<Escape>',lambda e: parent.destroy())
 
--- .vs/IA/Restroom Log/SheetsScript.py
+++ .vs/IA/Restroom Log/SheetsScript.py
@@ -1,3 +1,9 @@
-print("goodluck rohan")
+from time import *
 
+t = localtime()
+current_time = strftime("%H:%M:%S", t)
 
+def printstudent(RL):
+    student = RL
+    student= student.replace("\n", " ")
+    print(current_time + student)
