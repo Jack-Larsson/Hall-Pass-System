f4b758458ba4c7ea962a944d7486f1c311b4f2b5
Jack-Larsson <jacklarsson23@gmail.com>
2023-01-24 15:32:43 -0600

    sheets script updated

--- .vs/IA/Restroom Log/SheetsScript.py
+++ .vs/IA/Restroom Log/SheetsScript.py
@@ -1,9 +1,15 @@
 from time import *
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+gc = gspread.service_account(filename='mycredentials.json')
+gsheet = gc.open_by_key("1oMakpyyRqNZyMroZ-P6pcBn3dmfjeccDAPllFPeg9xc")
+wsheet = gsheet.worksheet("Sheet1")
+mydata = wsheet.get_all_records()
 
 t = localtime()
 current_time = strftime("%H:%M:%S", t)
 
-def printstudent(RL):
-    student = RL
-    student= student.replace("\n", " ")
-    print(current_time + student)
+def printstudent(last, first, timein,timeout):
+    wsheet.inset_row(last, first, timein, timeout)
