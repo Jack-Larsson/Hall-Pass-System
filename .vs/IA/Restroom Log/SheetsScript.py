dab3c4dbe48ebfebae81644465a76618a9ac7397
Jack-Larsson <Jacklarsson23@gmail.com>
2023-02-22 19:09:19 -0600

    make worksheet for use everymorning

--- .vs/IA/Restroom Log/SheetsScript.py
+++ .vs/IA/Restroom Log/SheetsScript.py
@@ -1,17 +1,25 @@
 from time import *
+from datetime import date
+from schedule import *
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+t = localtime()
+today = date.today()
+today.strftime('%m/%d/%Y')
+current_time = strftime("%H:%M:%S", t)
 
 gc = gspread.oauth(
-    credentials_filename=r'D:\s1649121\Documents\VS Projects\PublicEnemy1\.vs\IA\Restroom Log\googlecreds.json'
+    credentials_filename=r'D:\s1649121\Documents\VS Projects\PublicEnemy1\.vs\IA\Restroom Log\creds.json'
 )
-gsheet = gc.open_by_key("1oMakpyyRqNZyMroZ-P6pcBn3dmfjeccDAPllFPeg9xc")
-wsheet = gsheet.worksheet("Sheet1")
-mydata = wsheet.get_all_records()
 
-t = localtime()
-current_time = strftime("%H:%M:%S", t)
+gsheet = gc.open_by_key("1oMakpyyRqNZyMroZ-P6pcBn3dmfjeccDAPllFPeg9xc")
+
+def TodaysSheet():
+    worksheet = gsheet.add_worksheet(title= today, rows=100, cols=5)
+
+every().day.at("8:30").do(TodaysSheet())
+wsheet = gsheet.worksheet(today)
 
 def printstudent(last, first):
     wsheet.append_row([last, first, current_time])
